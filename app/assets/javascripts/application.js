80d668ac9cf91def482c56b50b86f15c8759efdd
Jacques <jacques.leroux3@gmail.com>
2020-06-19 17:07:39 +0200

    add content and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,21 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
+
+$( document ).on('turbolinks:load', function() {
+// Strikethrough
+$('a').addClass('strikethrough').hover(
+    (e) => $(e.target).stop().addClass('mouseenter'),
+    (e) => {
+        $(e.target).stop().removeClass('mouseenter');
+        setTimeout(() => {
+            $(e.target).stop().removeClass('mouseleave').addClass('no-transition');
+        }, STIKETHROUGH_DURATION - STIKETHROUGH_OFFSET);
+        setTimeout(() => {
+            $(e.target).stop().removeClass('no-transition');
+        }, STIKETHROUGH_DURATION + STIKETHROUGH_OFFSET);
+    }
+);
+
+// Strikethrough End
+})
