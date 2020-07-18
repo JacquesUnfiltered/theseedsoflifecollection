a429859276ce699489391abac95bb48fc1057d30
Jacques <jacques.leroux3@gmail.com>
2020-07-18 19:32:35 +0200

    New Start

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,26 +5,10 @@ class SimplePagesController < ApplicationController
   def about
   end
 
+  def shop
+  end
+
   def contact
   end
-
-  def download_cv
-  send_file(
-    "#{Rails.root}/public/cv-jacques-le-roux.pdf",
-    filename: "CV - Jacques le Roux.pdf",
-    type: "application/pdf"
-  )
-  end
-
-
-  # def thank_you
-  #   @name = params[:name]
-  #   @email = params[:email]
-  #   @message = params[:message]
-  #   ActionMailer::Base.mail(form: @email,
-  #                             to: 'jacques.leroux3@gmail.com',
-  #                        subject: "A new email from #{@name}",
-  #                           body: @message).deliver_now
-  # end
-
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/download_cv'
+  get 'simple_pages/shop'
   root 'simple_pages#index'
   # post 'simple_pages/thank_you'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
