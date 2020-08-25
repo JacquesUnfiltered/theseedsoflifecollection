c0e34cfc07f605380b9a72ad441db7eccd3bf08c
Jacques <jacques.leroux3@gmail.com>
2020-08-25 20:05:20 +0200

    change domain for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,7 +115,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'theseedsoflifecollection.heroku.com',
+    :domain         => 'theseedsoflifecollection.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
