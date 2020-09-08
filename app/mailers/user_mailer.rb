508a344114396caedd9d34e2c61997e57c5879a6
Jacques <jacques.leroux3@gmail.com>
2020-09-08 21:59:46 +0200

    add global vairables to email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,7 @@ class UserMailer < ActionMailer::Base
   def contact_form(name, email, message)
     @message = message
     @email = email
+    @name = name
     mail(from: 'hello@theseedsoflifecollection.co.za', 
         to: 'hello@theseedsoflifecollection.co.za', 
         subject: "A new contact form message from #{name}")
