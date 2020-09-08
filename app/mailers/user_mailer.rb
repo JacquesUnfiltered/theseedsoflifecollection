19bed2040b66f8fc6f24ef00654ca1526986f91f
Jacques <jacques.leroux3@gmail.com>
2020-09-08 22:18:20 +0200

    optimize responsiveness

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "hello@theseedsoflifecollection.co.za"
 
-  def contact_form(name, email, message)
+  def contact_form(email, name, message)
     @message = message
     @email = email
     @name = name
     mail(from: 'hello@theseedsoflifecollection.co.za', 
         to: 'hello@theseedsoflifecollection.co.za', 
-        subject: "A new contact form message from #{email}")
+        subject: "A new contact form message from #{name}")
   end
 end
