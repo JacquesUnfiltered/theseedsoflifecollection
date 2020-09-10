5460effdfca3a6eb70b79c85d639456b32e8f19f
Jacques <jacques.leroux3@gmail.com>
2020-09-10 21:23:49 +0200

    link the name on index page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,6 @@ class UserMailer < ActionMailer::Base
   def contact_form(email, name, message)
     @message = message
     @email = email
-    @name = name
     mail(from: 'hello@theseedsoflifecollection.co.za', 
         to: 'hello@theseedsoflifecollection.co.za', 
         subject: "A new contact form message from #{name}")
